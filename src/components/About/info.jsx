8c35b7dd5e5b205d70e9db278a20c970560f7514
nicklaranjeiro <nicholaslaranjeiro@hotmail.com>
2024-07-31 19:01:18 -0400

    Updated info in about section

--- src/components/About/info.jsx
+++ src/components/About/info.jsx
@@ -6,7 +6,7 @@ const Info = () => {
       <div className="about_box">
         <i className="bx bx-award about_icon"></i>
         <h3 className="about_title">Experience</h3>
-        <span className="about_subtitle">3 + Years Experience</span>
+        <span className="about_subtitle">5 + Years Experience</span>
       </div>
       <div className="about_box">
         <i className="bx bx-briefcase-alt about_icon"></i>
